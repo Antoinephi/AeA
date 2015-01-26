8fe2d3c7bed0b437361dd5f1f6d676cb28cd8572
Mandrivia <antoine.philippe62@gmail.com>
2015-01-26 13:50:59 +0100

    fin séance 2

--- tp1/src/RechercheMotif/KMP.java
+++ tp1/src/RechercheMotif/KMP.java
@@ -5,47 +5,81 @@ public class KMP {
 	char[] sequence;
 	public int[] next;
 	char[] motif;
-	
-	
-	public KMP(char[] sequence, char[] motif){
+
+	public KMP(char[] sequence, char[] motif) {
 		this.sequence = sequence;
 		this.motif = motif;
 	}
-	
-	public void calculNext(){
-		this.next = new int[this.motif.length];
+
+	public void calculNext() {
+		this.next = new int[this.motif.length+1];
 		this.next[0] = -1;
-		this.next[1] = motif[0] == motif[1] ? -1 : 0; 
+		this.next[1] = motif[0] == motif[1] ? -1 : 0;
 		int j;
 		int estTrouve = 0;
+		int estUnBord = 0;
 		for (int i = 2; i < this.next.length; i++) {
-		j = i - 2;	
-			while((j > 0) && (estTrouve == 0)){
-					int estUnBord = 1;
-					for(int k = 0; k <= j; k++){
-						if(this.motif[k] != this.motif[i-k]){
-							estUnBord = 0;
-							break;
+			estTrouve = 0;
+			j = i - 1;
+			while ((j >= 0) && (estTrouve == 0)) {
+				estUnBord = 1;
+				for (int k = 0; k <= j; k++) {
+					if (this.motif[k] != this.motif[i-j+k-1]) {
+						estUnBord = 0;
+						if(i==6){
 						}
 					}
-					if(estUnBord == 1){
-						if(this.motif[j+1] == this.motif[i]){
+				}
+				if (estUnBord == 1 && i <next.length-1) {
+					if (this.motif[j + 1] != this.motif[i]) {
+						estTrouve = 1;
+					}
+				} else 
+					if(estUnBord == 1 && i == next.length-1){
+						if(j!=i-1)
 							estTrouve = 1;
-						}
 					}
-					j--;
+				j--;
 			}
-			this.next[i] = j+1;
+			if (j == -1 && i < next.length-1) {
+
+				if (this.motif[0] == this.motif[i]) {
+					j = -3;
+				} else {
+					j = -2;
+				}
+			} else 
+				{
+					if(j == -1 && i == next.length-1){
+						j = -2;
+					}
+				}
+			this.next[i] = j + 2;
 		}
 	}
 	
+	public void algo(){
+		int indice = 0;
+		for (int i = 0; i < this.sequence.length-this.motif.length; i++) {
+			if(this.sequence[i] == this.motif[indice]) {
+				indice++;
+			} else {
+				indice = 0;
+				i-=this.next[indice]-1;
+			}
+			if(indice == this.motif.length-1)
+				System.out.println("mot trouvé");
+		}
+		
+	}
+
 	public static void main(String[] args) {
-		char[] motif = {'T','A','C','T','A','G','A'};
+		char[] motif = {'T','A','C', 'T', 'A', 'G', 'A'};
 		KMP recherche = new KMP(null,motif);
 		recherche.calculNext();
-		
+		recherche.algo();
 		for(int i=0;i<recherche.next.length;i++){
-			System.out.println(recherche.next[i]+" ");
+			System.out.print(recherche.next[i]+" ");
 		}
 		
 		
