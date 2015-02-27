36b2455b0030be0d404d1b3832b70fa14a30017e
Delphine <delphine.poux@gmail.com>
2015-02-27 10:23:54 +0100

    avancée tp2

---
+++ tp2/src/RechercheMotif/AlgoRecherche.java
@@ -0,0 +1,83 @@
+package RechercheMotif;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Conversion.ConvertSequence;
+
+public abstract class AlgoRecherche implements Algorithme {
+
+	protected char[] sequence;
+	protected char[] motif;
+	protected List<Integer> occurences;
+	
+	
+	public AlgoRecherche(char[] sequence, char[] motif){
+		this.sequence = sequence;
+		this.motif = motif;
+		occurences = new ArrayList<Integer>();
+	}
+	
+	public void setMotif(char[] motif){
+		this.motif = motif;
+	}
+	
+	public void resetListOccurences(){
+		this.occurences = new ArrayList<Integer>();
+	}
+	
+	public List<Integer> getListOccurences(){
+		return this.occurences;
+	}
+	
+	public void calculTous(int n){
+		try {
+			PrintWriter p = new PrintWriter("file.txt", "UTF-8");
+			for(int i=0;i<this.sequence.length-this.motif.length-1;i++){
+				this.resetListOccurences();
+				char[] motif_n = new char[n];
+				for(int j=0;j<n;j++){
+					motif_n[j] = this.sequence[i+j];
+				}
+				ConvertSequence seq = new ConvertSequence(motif_n);
+				this.setMotif(motif_n);
+				this.calcul();
+				for(int k = 0;k<this.occurences.size();k++){
+					p.println(i + " " + this.occurences.get(k));
+				}
+				this.setMotif(seq.getReverse());
+				this.calcul();
+				for(int k = 0;k<this.occurences.size();k++){
+					p.println(i + " " + this.occurences.get(k));
+				}
+				this.setMotif(seq.getComp());
+				this.calcul();
+				for(int k = 0;k<this.occurences.size();k++){
+					p.println(i + " " + this.occurences.get(k));
+				}
+				this.setMotif(seq.getReverseComp());
+				this.calcul();
+				for(int k = 0;k<this.occurences.size();k++){
+					p.println(i + " " + this.occurences.get(k));
+				}
+
+			}
+			p.close();
+
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public abstract void preCalcul();
+	
+	public abstract void calcul();
+	
+}
--- tp2/src/Tests/Simulateur.java
+++ tp2/src/Tests/Simulateur.java
@@ -2,8 +2,6 @@ package Tests;
 
 public class Simulateur {
 	
-	private char[] microARN;
-	
 	public Simulateur(){
 
 	}
@@ -12,7 +10,7 @@ public class Simulateur {
 	public char[] genPreMiARNSansBoucle(){
 		char[] alphabet = {'A','C','U','G'};
 		char[] reverseAlphabet = {'U','G','A','C'};
-		int taille = (int)(Math.random() * (100-48)) + 48;
+		int taille = (int)(Math.random() * (100-70)) + 70;
 		System.out.println(taille);
 		char[] preMiARN = new char[taille]; 
 		int rdm;
@@ -29,11 +27,55 @@ public class Simulateur {
 	
 	/* Génère un pré microARN avec des boucles */
 	public char[] genPreMiARNBoucles(){
-		char[] preMiARN = genPreMiARNSansBoucle();
-		int nb = (int)(Math.random() * 100 - preMiARN.length);
-		for(int i=0;i<preMiARN.length;i++){
-			
+		char[] alphabet = {'A','C','U','G'};
+		char[] reverseAlphabet = {'U','G','A','C'};
+		/* Taille du preMiARN */
+		int taille = (int)(Math.random() * (100 - 70))+70;
+		System.out.println("Taille : "+taille);
+		char[] preMiARN = new char[taille];
+		
+		/* Calcul du nombre de nucléotides dans les boucles */
+		int nucleotidesSansBoucle = (int)(Math.random()*(taille - 48))+48;
+		if(nucleotidesSansBoucle%2 == 1){
+			nucleotidesSansBoucle--;
 		}
+		System.out.println("nucleotides appariés : "+nucleotidesSansBoucle);
+		int nucleotidesDansBoucle = taille - nucleotidesSansBoucle;
+		System.out.println("nucleotides dans boucle : "+nucleotidesDansBoucle);
+		
+		/* Calcul du nombre de nucléotides dans la boucle finale */
+		int max = 8;
+		int min = 0;
+		if(nucleotidesDansBoucle < 8)
+			max = nucleotidesDansBoucle;
+		int nucleotidesBoucleFinale = (int)(Math.random() * (max - min))+min;
+		System.out.println("boucle finale : "+nucleotidesBoucleFinale);
+		
+		/* Création du preMiARN */
+		/* Insertion de la boucle finale */
+		int i = taille/2-nucleotidesBoucleFinale/2;
+		int j = i+nucleotidesBoucleFinale;
+		for(int k = i;k<j;k++){
+			int rdm = (int)(Math.random() * 3);
+			preMiARN[k] = alphabet[rdm];
+		}
+		i--;
+		j++;
+		
+		/* Insertion du reste des nucléotides */
+		while(i!=0 && j!=taille-1){
+			int rdm = (int)(Math.random() * (nucleotidesSansBoucle - 3))+3;
+			for(int m=0;m<rdm;m++){
+				int nucleoRdm = (int)(Math.random() * 3);
+				preMiARN[i] = alphabet[nucleoRdm];
+				preMiARN[j] = reverseAlphabet[nucleoRdm];
+				nucleotidesSansBoucle -= 2;
+			}
+		}
+		for(int l=0;l<preMiARN.length;l++){
+			System.out.println(l+" : "+" "+preMiARN[l]);
+		}
+		
 		return preMiARN;
 	}
 }
