d3f469013a6185e0258203e3b4696407bf73e853
Delphinep <delphine.poux@gmail.com>
2015-03-01 23:50:18 +0100

    fin de la classe check OK

--- tp2/src/Tests/Main.java
+++ tp2/src/Tests/Main.java
@@ -4,7 +4,13 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Simulateur sim = new Simulateur();
-		char[] preMiARN = sim.genPreMiARNBoucles();
+		char[] preMiARN = sim.genPreMiARNBoucles(true);
+		CheckSimulateur check = new CheckSimulateur(preMiARN);
+		System.out.println("Taille : "+check.checkSize());
+		System.out.println("Nombre d'appariements correct : "+check.enoughMatching());
+		System.out.println("Ordre des appariements correct : "+ check.goodMatching());
+		System.out.println("Au moins trois appariements : "+check.atLeastThreeMatching());
+		System.out.println("Nombre de nucléotides dans les boucles correct : "+ check.nbInLoop());
 	}
 	
 }
