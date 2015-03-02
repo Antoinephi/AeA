e0ff18e887ee96aff843fa1e138e0358888a5c54
Delphinep <delphine.poux@gmail.com>
2015-03-02 23:42:06 +0100

    début algo Needleman&Wunsch

--- tp2/src/RechercheMotif/RercherchePreMiARN.java
+++ tp2/src/RechercheMotif/RercherchePreMiARN.java
@@ -20,6 +20,20 @@ public class RercherchePreMiARN {
 		}		
 	}
 	
+	public void algoNeedlemanAndWunsch(){
+		int[][] table = new int[this.sequence.length/2][this.sequence.length/2];
+		table[0][0] = 0;
+		for(int i=1;i<table.length;i++){
+			table[i][0] = -2*i;
+			table[0][i] = -2*i;
+		}
+		for(int j=1;j<table.length;j++){
+			for(int k=1;k<table.length;k++){
+			//int sub = table[j-1][k-1] + ???;
+			}
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		char[] sequence = {'A', 'G', 'C', 'U', 'T', 'R', 'L'};
