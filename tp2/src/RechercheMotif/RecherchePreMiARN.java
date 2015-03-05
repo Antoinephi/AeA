8a2d2804415be33f92e30063a7515f67ac0a5d50
Delphinep <delphine.poux@gmail.com>
2015-03-05 22:48:32 +0100

    avancée tp2 indice remontée à corriger

--- tp2/src/RechercheMotif/RecherchePreMiARN.java
+++ tp2/src/RechercheMotif/RecherchePreMiARN.java
@@ -8,19 +8,6 @@ public class RecherchePreMiARN {
 		this.sequence = sequence;
 	}
 
-	/* A retirer avant de rendre ? */
-	public void calcul() {
-		for (int i = this.sequence.length - (this.sequence.length / 2 + 1), j = this.sequence.length / 2; i >= 0
-				&& j <= this.sequence.length; i--, j++) {
-
-			System.out.println(this.sequence[i] + ":" + i + " "
-					+ this.sequence[j] + ":" + j);
-			if (this.sequence[j] == this.sequence[i])
-				System.out.println("paire");
-
-		}
-	}
-
 	/**
 	 * Verifie si les 2 nucléotides sont des complémentaires ou non via un
 	 * parcours de tableau à 2 dimensions chaque dimension contenant un des
@@ -51,25 +38,29 @@ public class RecherchePreMiARN {
 	 */
 	public char[] algoNeedlemanAndWunsch() {
 		int[][] table;
-		System.out.println(this.sequence.length / 2 + 1);
-		table = new int[(this.sequence.length / 2) + 1][(this.sequence.length / 2) + 1];
-		int[][] tableI = new int[(this.sequence.length / 2 + 1)][(this.sequence.length / 2) + 1];
-		int[][] tableJ = new int[(this.sequence.length / 2 + 1)][(this.sequence.length / 2) + 1];
+		int tailleSurDeux = this.sequence.length / 2;
+		int taille = this.sequence.length;
+		table = new int[tailleSurDeux + 1 + taille % 2][tailleSurDeux + 1];
+		int[][] tableI = new int[tailleSurDeux + 1 + taille % 2][tailleSurDeux + 1];
+		int[][] tableJ = new int[tailleSurDeux + 1 + taille % 2][tailleSurDeux + 1];
 		int sub, del, ins;
 		table[0][0] = 0;
 		/* Initialisation du tableau */
 		for (int i = 1; i < table.length; i++) {
 			table[i][0] = -2 * i;
+		}
+		for (int i = 1; i < table[0].length; i++) {
 			table[0][i] = -2 * i;
 		}
+
 		for (int i = 1; i < table.length; i++) {
-			for (int j = 1; j < table.length; j++) {
+			for (int j = 1; j < table[0].length; j++) {
 				// sub = table[i-1][j-1] +
 				// (this.estUnAppariement(this.sequence[i],
 				// this.sequence[this.sequence.length-j]) ? 2 : -1);
 				sub = table[i - 1][j - 1]
 						+ (this.estUnAppariement(this.sequence[table.length - i
-								- 1], this.sequence[table.length + j - 2]) ? 2
+								- 1], this.sequence[table[0].length + j - 2]) ? 2
 								: -1);
 				del = table[i - 1][j] - 2;
 				ins = table[i][j - 1] - 2;
@@ -91,7 +82,7 @@ public class RecherchePreMiARN {
 			}
 		}
 		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table.length; j++) {
+			for (int j = 0; j < table[0].length; j++) {
 				System.out.print(table[i][j] + " | ");
 			}
 			System.out.println();
@@ -99,7 +90,7 @@ public class RecherchePreMiARN {
 		}
 		System.out.println();
 		for (int i = 0; i < table.length; i++) {
-			for (int j = 0; j < table.length; j++) {
+			for (int j = 0; j < table[0].length; j++) {
 				System.out.print("i:" + tableI[i][j] + " j:" + tableJ[i][j]
 						+ " | ");
 			}
@@ -114,75 +105,53 @@ public class RecherchePreMiARN {
 		}
 
 		/* Remontée */
-		int iMax = 0;
-		int jMax = 0;
-		int max = -2;
+		int iCurrent = table.length-1;
+		int jCurrent = table[0].length-1;
+		int iMax = tableI.length;
+		int jMax = tableJ[iCurrent].length;
 		char[] alignement = new char[this.sequence.length];
-		for (int i = 1; i < table.length; i++) {
-			if (table[i][table[0].length - 1] > max) {
-				max = table[i][table[0].length - 1];
-				iMax = i;
-				jMax = table[0].length - 1;
-			}
-		}
-		for (int j = 1; j < table[0].length; j++) {
-			if (table[table[0].length - 1][j] > max) {
-				max = table[table[0].length - 1][j];
-				iMax = table[0].length - 1;
-				jMax = j;
-			}
-		}
-		System.out.println("max : " + max);
-		System.out.println("i : " + iMax);
-		System.out.println("j : " + jMax);
+
+		/*
+		 * int max = -2; char[] alignement = new char[this.sequence.length]; for
+		 * (int i = 1; i < table.length; i++) { if (table[i][table[0].length -
+		 * 1] > max) { max = table[i][table[0].length - 1]; iMax = i; jMax =
+		 * table[0].length - 1; } } for (int j = 1; j < table[0].length; j++) {
+		 * if (table[table[0].length - 1][j] > max) { max =
+		 * table[table[0].length - 1][j]; iMax = table[0].length - 1; jMax = j;
+		 * } } System.out.println("max : " + max); System.out.println("i : " +
+		 * iMax); System.out.println("j : " + jMax);
+		 */
 		for (int k = 0; k < this.sequence.length; k++) {
 			alignement[k] = this.sequence[k];
 		}
+		int cpt = 0;
 		while (iMax != 0 || jMax != 0) {
-			if (tableI[iMax][jMax] == iMax) {
-				alignement[table.length + 1 - iMax] = '_';
-				jMax--;
-			} else if (tableJ[iMax][jMax] == jMax) {
-				alignement[table.length / 2 + jMax] = '_';
-				iMax--;
+			System.out.println(cpt);
+			cpt++;
+			if ((tableI[iCurrent][jCurrent] == iMax)) {
+				jMax = tableJ[iCurrent][jCurrent];
+				alignement[table.length - jMax-2] = '_';
+				jCurrent--;
+			} else if (tableJ[iCurrent][jCurrent] == jMax) {
+				System.out.println("ici");
+				iMax = tableI[iCurrent][jCurrent];
+				alignement[table[0].length - iMax - 1] = '_';
+				iCurrent--;
 			} else {
-				if (table[iMax][jMax] == table[iMax - 1][jMax - 1] - 1) {
-					alignement[alignement.length / 2 - iMax] = '_';
-					alignement[alignement.length / 2 + jMax - 1] = '_';
+				if (table[iCurrent][jCurrent] == table[iCurrent - 1][jCurrent - 1] - 1) {
+					alignement[table.length - iMax] = '_';
+					alignement[table[0].length +1 + jMax] = '_';
 				}
-				iMax--;
-				jMax--;
+				iMax = tableI[iCurrent][jCurrent];
+				jMax = tableJ[iCurrent][jCurrent];
+				iCurrent--;
+				jCurrent--;
 			}
 		}
 		for (int l = 0; l < alignement.length; l++) {
 			System.out.print(alignement[l] + " ");
 		}
 		System.out.println();
-		return alignement;
-	}
-
-	public char[] alignement(int[][] NandWresult) {
-		int iMax = 0;
-		int jMax = 0;
-		int max = 0;
-		char[] alignement = new char[this.sequence.length];
-		for (int i = 0; i < NandWresult.length; i++) {
-			if (NandWresult[i][NandWresult[0].length - 1] > max) {
-				max = NandWresult[i][NandWresult[0].length - 1];
-				iMax = i;
-				jMax = NandWresult[0].length - 1;
-			}
-		}
-		for (int j = 0; j < NandWresult[0].length; j++) {
-			if (NandWresult[NandWresult[0].length - 1][j] > max) {
-				max = NandWresult[NandWresult[0].length - 1][j];
-				iMax = NandWresult[0].length - 1;
-				jMax = j;
-			}
-		}
-		System.out.println("max : " + max);
-		System.out.println("i : " + iMax);
-		System.out.println("j : " + jMax);
 		return alignement;
 	}
 
@@ -195,7 +164,7 @@ public class RecherchePreMiARN {
 	 */
 	public boolean isPreMiARN(char[] alignement) {
 		/* Bonne taille */
-		if ((alignement.length < 70) || (alignement.length > 100)){
+		if ((alignement.length < 70) || (alignement.length > 100)) {
 			System.out.println("KO : Mauvaise taille");
 			return false;
 		}
@@ -207,7 +176,7 @@ public class RecherchePreMiARN {
 			if (alignement[i] != '_')
 				appariements++;
 		}
-		if (appariements < 48){
+		if (appariements < 48) {
 			System.out.println("KO : Pas assez de nucléotides appariés");
 			return false;
 		}
@@ -220,11 +189,13 @@ public class RecherchePreMiARN {
 		int i = 0;
 		while (i < alignement.length) {
 			if (alignement[i] != '_') {
-				if ((alignement[i + 1] == '_') || (alignement[i + 2] == '_')){
-					System.out.println("KO : Mauvais groupes pour les appariements");
+				if ((alignement[i + 1] == '_') || (alignement[i + 2] == '_')) {
+					System.out
+							.println("KO : Mauvais groupes pour les appariements "
+									+ i);
 					return false;
 				}
-				while ((i<alignement.length) && (alignement[i] != '_'))
+				while ((i < alignement.length) && (alignement[i] != '_'))
 					i++;
 			} else {
 				i++;
@@ -233,65 +204,70 @@ public class RecherchePreMiARN {
 		System.out.println("OK : Bons groupes pour les appariements");
 
 		/* Vérifie que la boucle finale a au plus 8 nucléotides */
-		i = alignement.length/2;
-		int j = alignement.length/2+1;
+		i = alignement.length / 2;
+		int j = alignement.length / 2 + 1;
 		int nbFinale = 0;
-		while(alignement[i] == '_'){
+		while (alignement[i] == '_') {
 			nbFinale++;
 			i--;
 		}
-		while(alignement[j] == '_'){
+		while (alignement[j] == '_') {
 			nbFinale++;
 			j++;
 		}
-		
-		if(nbFinale > 8){
-			System.out.println("KO : Trop de nucléotides dans la boucle finale");
+
+		if (nbFinale > 8) {
+			System.out
+					.println("KO : Trop de nucléotides dans la boucle finale");
 			return false;
 		}
-		System.out.println("OK : Bon nombre de nucléotides dans la boucle finale");
+		System.out
+				.println("OK : Bon nombre de nucléotides dans la boucle finale");
 		/* Vérifie que les autres boucles ont au plus 3 nucléotides */
 		/* A gauche de la boucle */
-		while(i>=0){
-			if(alignement[i] == '_'){
+		while (i >= 0) {
+			if (alignement[i] == '_') {
 				int nb = 0;
-				while(alignement[i] == '_'){
+				while (alignement[i] == '_') {
 					nb++;
 					i--;
 				}
-				if(nb>3){
-					System.out.println("KO : Trop de nucléotides dans une boucle à gauche");
+				if (nb > 3) {
+					System.out
+							.println("KO : Trop de nucléotides dans une boucle à gauche");
 					return false;
 				}
 			} else {
 				i--;
 			}
 		}
-		System.out.println("OK : Bon nombre de nucléotides dans les boucles à gauche");
+		System.out
+				.println("OK : Bon nombre de nucléotides dans les boucles à gauche");
 		/* Et à droite */
-		while(j<alignement.length){
-			if(alignement[j] == '_'){
+		while (j < alignement.length) {
+			if (alignement[j] == '_') {
 				int nb = 0;
-				while(alignement[j] == '_'){
+				while (alignement[j] == '_') {
 					nb++;
 					j++;
 				}
-				if(nb>3){
-					System.out.println("KO : Trop de nucléotides dans une boucle à droite");
+				if (nb > 3) {
+					System.out
+							.println("KO : Trop de nucléotides dans une boucle à droite");
 					return false;
 				}
 			} else {
 				j++;
 			}
 		}
-		System.out.println("OK : Bon nombre de nucléotides dans les boucles à droite");
+		System.out
+				.println("OK : Bon nombre de nucléotides dans les boucles à droite");
 		return true;
 	}
 
 	public static void main(String[] args) {
-		char[] sequence = { 'U', 'A', 'U', 'C' };
-		char[] sequence1 = { 'C', 'U', 'A', 'C', 'A', 'G', 'U', 'A', 'G', 'C',
-				'C', 'G', 'A', 'U', 'A' };
+		char[] sequence = { 'A', 'A', 'U' };
+		char[] sequence1 = { 'C', 'U', 'A', 'C', 'G', 'A', 'U', 'A' };
 		char[] sequence2 = { 'A', 'U', 'A', 'G', 'C', 'C', 'G', 'U', 'A', 'C',
 				'U', 'G', 'U', 'A', 'G', 'G' };
 		char[] sequence3 = { 'A', 'A' };
@@ -300,4 +276,10 @@ public class RecherchePreMiARN {
 		System.out.println(algo.isPreMiARN(result));
 	}
 
+	/*
+	 * int[][] test = new int[5/2+5%2][5/2]; for(int i=0;i<test.length;i++){
+	 * for(int j=0;j<test[0].length;j++){ test[i][j] = 1; } } for(int
+	 * i=0;i<test.length;i++){ for(int j=0;j<test[0].length;j++){
+	 * System.out.print(test[i][j] + " "); } System.out.println(); } }
+	 */
 }
