49c8e49c4e2f6ed423b00d1d8b00c15b8f10a696
Mandrivia <antoine.philippe62@gmail.com>
2015-03-13 10:40:52 +0100

    exo 1 fini

--- lettreQuiSaute/src/Graphe.java
+++ lettreQuiSaute/src/Graphe.java
@@ -32,7 +32,7 @@ public class Graphe {
 		// Recherche du dernier élément de la liste
 		Liste lastS = this.lastSucc(s);
 		Liste lastD = this.lastSucc(d);
-
+		
 		// Ajout de l'arete entre S et D
 		lastS.setNextElement(nextS);
 		lastD.setNextElement(nextD);
@@ -44,50 +44,55 @@ public class Graphe {
 	 * @param s
 	 * @return
 	 */
-	@SuppressWarnings("finally")
 	private Liste lastSucc(int s) {
 		Liste current = this.listeSucc[s];
-		try {
 			while (current.getNextElement() != null) {
 				current = current.getNextElement();
 			}
-		} finally {
-			return current;
-		}
+		//System.out.println(s  + " ---> " + current.getElement() + ":" + this.lesMots[current.getElement()]);
+		return current;
 	}
 
 	public void lettreQuiSaute() {
 		for (int i = 0; i < this.nb; i++) {
 			for (int j = i+1; j < this.nb; j++) {
-				if (diffUneLettre(this.lesMots[i], this.lesMots[j]))
+				if (diffUneLettre(this.lesMots[i], this.lesMots[j])){
+					//System.out.println("ajouter : " + i + " " + j + " : " + this.lesMots[i] + " : " + this.lesMots[j]);
 					this.ajouterArete(i, j);
+				}
 			}
 		}
 
 	}
 
-	public void afficher(){
-		for(int i = 0; i < this.listeSucc.length; i++){
+	public void afficher() {
+		for (int i = 0; i < this.listeSucc.length; i++) {
 			Liste l = this.listeSucc[i];
-			while((l = l.getNextElement()) != null){
+			System.out.print(this.lesMots[l.getElement()] + " -> ");
+			while ((l = l.getNextElement()) != null) {
 				System.out.print(this.lesMots[l.getElement()] + " -> ");
 			}
 			System.out.println();
 		}
 	}
-	
-	private boolean diffUneLettre(String a, String b) {
+
+	public boolean diffUneLettre(String a, String b) {
 		// a et b supposees de meme longueur
 		int i = 0;
+		// parcouru du mot a la recherche d'une difference
 		while (i < a.length() && a.charAt(i) == b.charAt(i))
 			++i;
+		// on a parcouru le mot complet sans trouver de difference
 		if (i == a.length())
 			return false;
 		++i;
+		// une difference a ete trouve, recherche d'une seconde difference
 		while (i < a.length() && a.charAt(i) == b.charAt(i))
 			++i;
+		// le mot complet a ete parcouru et pas de seconde difference trouvee
 		if (i == a.length())
 			return true;
+		// une deuxieme difference a ete trouvee
 		return false;
 	}
 
@@ -96,6 +101,8 @@ public class Graphe {
 				"gnu", "glu", "gui", "guy", "gre", "gue", "ace", "acm", "agi",
 				"ait", "aie", "ail", "air", "and", "alu", "ami", "arc", "are",
 				"art", "apr", "avr", "sur", "mat", "mur" };
+		String[] dicocourt = { "gag", "gai", "gaz", 
+				"gnu", "glu", "gui", "guy"};
 		Graphe g = new Graphe(Dicos.dico5);
 		g.lettreQuiSaute();
 		g.afficher();
